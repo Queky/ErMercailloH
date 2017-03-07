6b366e84f859e3edc980e2de7f0432fb7156ce4d
Iñaki Sánchez <inakisanchez_92@hotmail.com>
2017-03-07 18:54:26 +0100

    cambios

--- app/src/main/java/com/example/eneko/ermercailloh/MainActivity.java
+++ app/src/main/java/com/example/eneko/ermercailloh/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,6 +70,12 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        else if(id == R.id.action_buscar){
+            Toast toast1 =
+                    Toast.makeText(getApplicationContext(), "Buscar articulo", Toast.LENGTH_SHORT);
+            toast1.show();
         }
 
         return super.onOptionsItemSelected(item);
